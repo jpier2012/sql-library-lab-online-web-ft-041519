982b4a51afff71e1eb17693a937955bfb3a9db32
James Pier <jpier2012@gmail.com>
2019-05-16 17:35:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,11 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "title", 1, 1)
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, "other title", 2, 2)
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "title", 1, 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", "1989", 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", "1989", 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", "1989", 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", "1989", 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", "1989", 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", "1989", 1)
+
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (1, "title", 1, 1)
