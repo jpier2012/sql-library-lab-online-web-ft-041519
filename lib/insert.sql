3e462b0eedaba3a3fd20f2f84e19ab925bbea73e
James Pier <jpier2012@gmail.com>
2019-05-16 17:34:18 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "title", 1, 1)
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, "other title", 2, 2)
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "title", 1, 1)
