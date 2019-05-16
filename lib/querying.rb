cbf7d34034299e592742a96e64daa02e1c4d9af9
James Pier <jpier2012@gmail.com>
2019-05-16 18:47:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,9 +27,16 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters ON series.id = characters.series_id
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*)
+  FROM characters
+  INNER JOIN character_books ON characters.id = character_books.character_id
+  GROUP BY characters.name;"
 end
